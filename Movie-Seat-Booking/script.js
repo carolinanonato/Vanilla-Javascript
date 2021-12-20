6d016116289ff51ab118820333456c10a5f45083
Carolina Nonato <csnonato@gmail.com>
2021-12-20 17:10:39 -0500

    save data to local storage + populate UI

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -6,14 +6,58 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value; 
 
+populateUI();
+
+//save selected movie index and price
+
+function setMovieData (movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+
 // Update total and count
 function updateSelectedCount () {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    
+
+    //Copy selected seats into arr
+    //Map throught array
+    //return a new array indexes
+
+    const seatsIndex = [...selectedSeats].map(function(seat) {
+        return [...seats].indexOf(seat);
+    });
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount*ticketPrice;
 }
+
+
+// get data from local storage and populateUI
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+};
+
+
+
 
 //Movie select event
 movieSelect.addEventListener('change', e=> {
@@ -28,4 +72,7 @@ container.addEventListener('click', e => {
 
         updateSelectedCount();
     }
-})
+});
+
+//initial count and total set
+updateSelectedCount();
